55918ded24b35ec6afd1d0c209e327454ee974e8
venutho <venu.t@comakeit.com>
2023-04-06 13:14:22 +0530

    added 2 test cases

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,12 @@
 Calculator functions
 """
 
+
 def add(x,y):
     """Add x and y and return result."""
     return x+y
+
+
+def subtract(x,y):
+    """Subtract y from x and return the result."""
+    return x-y
--- app/app/test.py
+++ app/app/test.py
@@ -8,9 +8,16 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
-   
+
     def test_add_numbers(self):
         """Test adding numbers together"""
-        res=calc.add(10,20)
+        res = calc.add(10, 20)
 
-        self.assertEqual(res,30)
+        self.assertEqual(res, 30)
+
+
+    def test_subtract_numbers(self):
+        """Test subtracting one numbers from another"""
+        res = calc.subtract(20, 10)
+
+        self.assertEqual(res, 10)
